4014a60795585abf57169b733666c8afd5ddbf9d
Alex 'Ript' Malyshev <alexript@gmail.com>
2011-09-26 14:28:35 +0400

    fix commits

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-     //   "bytes"
+	//   "bytes"
         "fmt"
         "http"
-    //    "image"
-     //   "image/jpeg"
-       // _ "image/png" // import so we can read PNG files.
+	//    "image"
+	//   "image/jpeg"
+	// _ "image/png" // import so we can read PNG files.
         "io"
-		"io/ioutil"
-       // "json"
+	"io/ioutil"
+	// "json"
         "os"
         //"strconv"
         "old/template"
@@ -18,7 +18,7 @@ import (
 // These imports were added for deployment on App Engine.
 import (
         "crypto/sha1"
- //       "resize"
+	//       "resize"
 )
 
 var (
@@ -49,49 +49,49 @@ func upload(w http.ResponseWriter, r *http.Request) {
         check(err)
         defer f.Close()
 
-		t, err := ioutil.TempFile(".", "lrg-")
-    check(err)
-    defer t.Close()
-    io.Copy(t, f)
-		
+	t, err := ioutil.TempFile(".", "lrg-")
+	check(err)
+	defer t.Close()
+	io.Copy(t, f)
+	
         // Grab the image data
-       // var buf bytes.Buffer
-      //  io.Copy(&buf, f)
-      //  i, _, err := image.Decode(&buf)
+	// var buf bytes.Buffer
+	//  io.Copy(&buf, f)
+	//  i, _, err := image.Decode(&buf)
         //check(err)
 
         // Resize if too large, for more efficient moustachioing.
         // We aim for less than 1200 pixels in any dimension; if the
         // picture is larger than that, we squeeze it down to 600.
-      //  const max = 1200
-       // if b := i.Bounds(); b.Dx() > max || b.Dy() > max {
-                // If it's gigantic, it's more efficient to downsample first
-                // and then resize; resizing will smooth out the roughness.
-         //       if b.Dx() > 2*max || b.Dy() > 2*max {
-           //             w, h := max, max
-             //           if b.Dx() > b.Dy() {
-               //                 h = b.Dy() * h / b.Dx()
-                 //       } else {
+	//  const max = 1200
+	// if b := i.Bounds(); b.Dx() > max || b.Dy() > max {
+        // If it's gigantic, it's more efficient to downsample first
+        // and then resize; resizing will smooth out the roughness.
+        //       if b.Dx() > 2*max || b.Dy() > 2*max {
+        //             w, h := max, max
+        //           if b.Dx() > b.Dy() {
+        //                 h = b.Dy() * h / b.Dx()
+        //       } else {
         //                        w = b.Dx() * w / b.Dy()
-          //              }
-            //            i = resize.Resample(i, i.Bounds(), w, h)
-              //          b = i.Bounds()
-                //}
-      //          w, h := max/2, max/2
+        //              }
+        //            i = resize.Resample(i, i.Bounds(), w, h)
+        //          b = i.Bounds()
+        //}
+	//          w, h := max/2, max/2
         //        if b.Dx() > b.Dy() {
-          //              h = b.Dy() * h / b.Dx()
-            //    } else {
-              //          w = b.Dx() * w / b.Dy()
-                //}
-      //          i = resize.Resize(i, i.Bounds(), w, h)
+        //              h = b.Dy() * h / b.Dx()
+        //    } else {
+        //          w = b.Dx() * w / b.Dy()
+        //}
+	//          i = resize.Resize(i, i.Bounds(), w, h)
         //}
 
         // Encode as a new JPEG image.
-   //     buf.Reset()
-     //   err = jpeg.Encode(&buf, i, nil)
-      //  check(err)
-		//t, err = Image{buf.Bytes()}
-		//check(err)
+	//     buf.Reset()
+	//   err = jpeg.Encode(&buf, i, nil)
+	//  check(err)
+	//t, err = Image{buf.Bytes()}
+	//check(err)
         // Create an App Engine context for the client's request.
         //c := appengine.NewContext(r)
 
@@ -101,8 +101,8 @@ func upload(w http.ResponseWriter, r *http.Request) {
         //check(err)
 
         // Redirect to /img using the key.
-   //     http.Redirect(w, r, "/img?id="+key.StringID(), http.StatusFound)
-   http.Redirect(w, r, "/img?id="+t.Name(), 302)
+	//     http.Redirect(w, r, "/img?id="+key.StringID(), http.StatusFound)
+	http.Redirect(w, r, "/img?id="+t.Name(), 302)
 }
 
 // keyOf returns (part of) the SHA-1 hash of the data, as a hex string.
@@ -125,8 +125,8 @@ func img(w http.ResponseWriter, r *http.Request) {
         //get := func(n string) int { // helper closure
         //        i, _ := strconv.Atoi(r.FormValue(n))
         //        return i
-       // }
-       // x, y, s, d := get("x"), get("y"), get("s"), get("d")
+	// }
+	// x, y, s, d := get("x"), get("y"), get("s"), get("d")
 
         //if x > 0 { // only draw if coordinates provided
         //        m = moustache(m, x, y, s, d)
@@ -134,8 +134,8 @@ func img(w http.ResponseWriter, r *http.Request) {
 
         //w.Header().Set("Content-type", "image/jpeg")
         //jpeg.Encode(w, m, nil)
-		w.Header().Set("Content-Type", "image")
-    http.ServeFile(w, r, r.FormValue("id"))
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, r.FormValue("id"))
 }
 
 func errorHandler(fn http.HandlerFunc) http.HandlerFunc {
