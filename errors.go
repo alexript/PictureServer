8bfdcd8aa2181e2a05eea1ae25a69ff38bbd0825
Alex 'Ript' Malyshev <alexript@gmail.com>
2011-09-27 19:52:08 +0400

    pre-panic console message

--- errors.go
+++ errors.go
@@ -6,6 +6,7 @@ import "os"
 // check aborts the current execution if err is non-nil.
 func Check(err os.Error) {
         if err != nil {
+		println(err.String())
                 panic(err)
         }
 }
